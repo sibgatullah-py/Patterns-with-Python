03065674c6fe0206cf328380f5be7456abedb4f6
Sibgatullah <sibgatullah881@gmail.com>
2025-11-18 15:24:59 +0600

    half diamond

---
+++ right side half diamond.py
@@ -0,0 +1,12 @@
+n = int(input())
+# top side
+for row in range(1,n+1):
+    for col in range(row):
+        print('*', end='')
+    print()
+    
+# bottom side
+for row in range(n-1,0,-1):
+    for col in range(row):
+        print('*',end='')
+    print()
